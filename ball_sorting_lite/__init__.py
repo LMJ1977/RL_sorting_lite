970b89f5a3cf53516cd4878511b348080f804b18
Luis Monzon Jaso <32835621+LMJ1977@users.noreply.github.com>
2022-08-02 11:47:24 +0200

    Update __init__.py

--- ball_sorting_lite/__init__.py
+++ ball_sorting_lite/__init__.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id='ball-sorting-lite-v0',
+    id='ball_sorting_lite-v0',
     entry_point='ball_sorting_lite.envs:BallSortingLiteEnv',
 )
